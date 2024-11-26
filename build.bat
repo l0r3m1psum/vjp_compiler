1520c36ec6c5de43e6b601076fa7a42ee9298b05
Diego Bellani <bellanidiego@gmail.com>
2024-11-26 10:58:50 +0100

    Added a few comments

--- build.bat
+++ build.bat
@@ -1,6 +1,7 @@
 @echo off
 
 rem https://devblogs.microsoft.com/cppblog/addresssanitizer-asan-for-windows-with-msvc/
+rem https://learn.microsoft.com/en-us/cpp/build/reference/d-preprocessor-definitions
 
 rem cl /nologo /Zi /fsanitize=address /std:c17 main.c
 cl /nologo /Zi /std:c17 main.c /JMC /link /natvis:types.natvis || exit /b
