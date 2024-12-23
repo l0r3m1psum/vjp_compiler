b69bd126fd9b059cd9dc7aebda9aaea19e67a2a2
Diego Bellani <bellanidiego@gmail.com>
2024-12-23 19:26:09 +0100

    Added small shell script to avoid opening a web browser

---
+++ matrixcalculus.sh
@@ -0,0 +1,19 @@
+IFS='' read -r -d '' payload <<'EOF'
+{
+	"expression": "x'*A*x + c*sin(y)'*x",
+	"wrt": {"name":"x","type":"vector"},
+	"varList": [
+		{"name":"A","type":"symmetric matrix"},
+		{"name":"c","type":"scalar"},
+		{"name":"x","type":"vector"},
+		{"name":"y","type":"vector"}
+	],
+	"n": 4
+}
+EOF
+
+curl 'https://www.matrixcalculus.org/_show' \
+	-s \
+	-X 'POST' \
+	-H 'Content-Type: application/json' \
+	-d  "$payload" | jq -M .derivative
